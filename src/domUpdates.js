7d5fc7b4039dc22f8b4720ed4dd6dd0190abf849
Denzel Turambi <dturambi26@gmail.com>
2023-06-12 10:19:40 -0600

    feat: Add nav buttons functionality

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,8 +1,20 @@
 import { getAllBookings, getAllCustomers, getAllRooms, getSingleCustomer } from "./apiCalls";
 
 // Query Selectors
-
+const newBookingButton = document.querySelector('.book-now-button');
+const profileButton = document.querySelector('.profile-button');
+const bookNowPage = document.querySelector('.book-now-page');
+const profilePage = document.querySelector('.profile-page');
 // Event Listeners
+newBookingButton.addEventListener('click', function () {
+    removeHiddenClass([bookNowPage]);
+    addHiddenClass([profilePage]);
+});
+
+profileButton.addEventListener('click', function () {
+  removeHiddenClass([profilePage]);
+  addHiddenClass([bookNowPage]);
+})
 
 // Event Handlers/Functions
 function removeHiddenClass(elements) {
